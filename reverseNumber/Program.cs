03cba246f4d5345fd2ca6d14c0b6a274ecc113d0
Ercan-home <ercanerdogan@gmail.com>
2020-03-25 17:43:25 +0300

    number has been reversed

---
+++ reverseNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace reverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int modNumber;
+            Console.Write("Reverse of number you entered : ");
+            while (number > 10)
+            {
+                modNumber = number % 10;
+                Console.Write(modNumber);
+                number = number / 10;
+            }
+
+            Console.Write(number);
+
+
+            Console.ReadLine();
+        }
+    }
+}
